38d0ffe3f990d15809391ac14f5b7e52c1a392cb
Nate Smith <59745316+generic-smith@users.noreply.github.com>
2021-04-27 17:42:10 -0400

    Update prim2dmaze.py

--- prim2dmaze.py
+++ prim2dmaze.py
@@ -1,5 +1,6 @@
 from random import random, randrange
 
+
 # key:
 # 0 = path
 # 1 = wall
@@ -8,43 +9,23 @@ from random import random, randrange
 # 4 = entrance to layer below
 # 5 = endpoint
 
-examplemaze10by10by3 = [
-    [[1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-     [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-     [1, 0, 1, 2, 1, 1, 1, 0, 1, 1, 1],
-     [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-     [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
-     [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
-     [1, 1, 0, 1, 1, 3, 0, 0, 1, 0, 1],
-     [0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1],
-     [0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-     [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]],
+'''
+    Mazes are represented using the Key above. Essential the maze is
+    designed such that every even pair of coordinates in the maze represents
+    a node. Every other pair of coordinates represents an edge connecting the 
+    nodes. The algorithm works by randomly selecting a starting node and then
+    expanding out from that node. Each as new nodes are added as paths in the 
+    maze, the surrounding walls of the node are added to the list of edges. 
+    Each round, an edge is randomly selected from that list. If turning that 
+    edge wall into a path would not result in a loop (i.e. it is not neighboring
+    more than 1 path square) that the edge and corresponding vertices are 
+    all made into path squares. This is repeated until there are no remaining 
+    edges/walls remaining in the list of neighboring walls/edges.
+'''
 
-    [[1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-     [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-     [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1],
-     [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-     [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
-     [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
-     [1, 1, 0, 1, 1, 4, 0, 0, 1, 0, 1],
-     [0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1],
-     [0, 1, 0, 1, 0, 3, 0, 0, 0, 0, 0],
-     [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]],
+class Point:  # this represents a single edge or node in the array. The parent of the
+              # Point is the preceding point which this point is connecting to.
 
-    [[1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
-     [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-     [1, 0, 1, 5, 1, 1, 1, 0, 1, 1, 1],
-     [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-     [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1],
-     [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0],
-     [1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1],
-     [0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1],
-     [0, 1, 0, 1, 0, 4, 0, 0, 0, 0, 0],
-     [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]],
-]
-
-
-class Point:
     def __eq__(self, o: object) -> bool:
         return isinstance(o, Point) and (self.x == o.x or self.y == o.y)
 
@@ -53,7 +34,7 @@ class Point:
         self.y = aY
         self.parent = parent
 
-    # compute opposite node given that it is in the other direction from the parent
+    # compute the coordinates of the node on the opposite side of this points from the parent
     def opposite(self):
         if self.x - self.parent.x != 0:
             return Point(self.x + (self.x - self.parent.x), self.y, self)
@@ -75,19 +56,23 @@ def gen2dmaze(width, height, xOdd=False, yOdd=False):
 
     walls = []
 
-    x = int(randrange(1 + (0 if xOdd else 1), width, 2))
-    y = int(randrange(1 + (0 if yOdd else 1), height, 2))
+    x = int(randrange(1 + (0 if xOdd else 1), width, 2))  # randomly generate a number to
+                                                          # be the initial x coordinate
+    y = int(randrange(1 + (0 if yOdd else 1), height, 2)) # randomly generate a number to
+                                                          # be the initial y coordinate
 
+    # add all of the walls neighboring the initial point into the walls list
     walls += [Point(x + 1, y, Point(x, y, None))]
     walls += [Point(x, y + 1, Point(x, y, None))]
     walls += [Point(x - 1, y, Point(x, y, None))]
     walls += [Point(x, y - 1, Point(x, y, None))]
-    maze[x][y] = 0
 
-    while len(walls) > 0:
-        wallIndex = int(random() * len(walls))
-        wallX = walls[wallIndex].x
-        wallY = walls[wallIndex].y
+    maze[x][y] = 0  # make the initial point a path
+
+    while len(walls) > 0:  # iterate until there are no more available edges
+        wallIndex = int(random() * len(walls))  # select a random edge from the list of viable edges
+        wallX = walls[wallIndex].x  # get the x coordinate of the wall
+        wallY = walls[wallIndex].y  # get the y coordinate of the wall
         opposite = walls[wallIndex].opposite()
         walls.pop(wallIndex)
 
