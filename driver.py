382d5a458474046b638ba6fe502054160757a75a
Nate Smith <59745316+generic-smith@users.noreply.github.com>
2021-04-27 17:41:23 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,40 +1,33 @@
+from random import random
+
 from gen3dmaze import gen3dMaze
 from maze_solver import solver, display_path
-from random import randrange
-import time
-from threading import Thread
 
-errors = 0
-runs = 10000
-for i in range(runs):
-    subtotal = 0
-    print(i)
+def errorTest(numTimes, verbose=False):
+    errors = 0
+    for x in range(numTimes):
+        if verbose:
+            print("round:", x)
+        maze = gen3dMaze(int(random() * 5 + 5), int(random() * 5 + 5), int(random() * 4 + 1))
+        # maze = gen3dMaze(int(4), int(5), int(3))
 
-    start = time.time()
-    # x = randrange(5, 10)
-    # y = randrange(5, 10)
-    # z = randrange(5, 10)
-    z = 10
-    x = 10
-    y = 10
-    print(x, y, z)
+        try:
+            solution = solver(maze)
+        except AttributeError as e:
+            print(e)
+        if solution is False:
+            errors += 1
+            print("Maze: ")
+            print(display_path(maze))
+            print("---------------------------------------------------------------------------------")
 
-    try:
-        maze = gen3dMaze(x, y, z)
-        print("Created maze")
-        print("Time: {}".format(time.time() - start))
-        start = time.time()
-        solution = solver(maze)
-        print("Solved")
-        solve_time = time.time() - start
-        subtotal += solve_time
-        print("Time: {}".format(solve_time))
-    except AttributeError as e:
-        print(e)
-    if solution is False:
-        errors += 1
-        print("Maze: ")
-        print(display_path(maze))
-        print("---------------------------------------------------------------------------------")
-print("avg solve time was: {}".format(subtotal / runs))
-print(errors, "errors found")
+    if verbose:
+        print(errors, "errors found")
+        if errors == 0:
+            print("maze generation successful for", str(numTimes), "tests")
+    return errors
+
+
+errorTest(10000, verbose=True)
+
+
